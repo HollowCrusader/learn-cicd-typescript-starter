207aa43592b158132f886063c2e651f2a486cd6f
Aaron Parker <ajpccl@rit.edu>
2025-12-17 11:51:40 -0500

    fix the workflow

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,7 @@ import { handlerUsersCreate, handlerUsersGet } from "./api/users.js";
 
 const __dirname = path.resolve();
 
-function unused() {
-  // this function does nothing
-}
+
 
 if (!config.api.port) {
   console.error("PORT environment variable is not set");
@@ -51,5 +49,3 @@ app.use("/v1", v1Router);
 app.listen(config.api.port, () => {
   console.log(`Server is running on port: ${config.api.port}`);
 });
-
-unused();
